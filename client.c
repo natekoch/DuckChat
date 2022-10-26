491260401a70d2232cc48fcc860c5eee393dde78
Nate Koch <31897172+natekoch@users.noreply.github.com>
2022-10-25 22:25:24 -0700

    input length checks

--- client.c
+++ client.c
@@ -13,11 +13,17 @@ int main(int argc, char *argv[]) {
     long port;
 
     if (argc == 4) {
+        if (strlen(argv[1]) > UNIX_PATH_MAX)
         hostname = malloc(strlen(argv[1]) + 1);
         strcpy(hostname, argv[1]);
 
         port = atol(argv[2]);
 
+        if (strlen(argv[3])  > USERNAME_MAX) {
+            // TODO: Figure out the expected client response from test binaries
+            printf("Username must be less than 32 bytes long.");
+            goto exit;
+        }
         username = malloc(strlen(argv[3]) + 1);
         strcpy(hostname, argv[3]);
     }
@@ -25,6 +31,22 @@ int main(int argc, char *argv[]) {
         printf("Usage: ./client server_socket server_port username\n");
         goto exit;
     }
+
+
+    //printf("%s", username);
+    // connect to server
+
+    // listen stdin for chat and commands
+
+    // print out any responses from server
+
+    /*
+    struct request_login *login;
+    login->req_type = REQ_LOGIN;
+    strcpy(login->req_username, argv);
+
+    printf("%d %s", login->req_type, login->req_username);
+    */
 
 exit:
     if (hostname != NULL) free(hostname);
